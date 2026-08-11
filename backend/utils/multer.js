08d30e2fd6b7debc464c9ccaf49f180c7eccc1bb
Nikita Chauhan <nikitachauhan2006@gmail.com>
2026-08-11 21:53:32 +0530

    Fix resume upload directory

--- backend/utils/multer.js
+++ backend/utils/multer.js
@@ -1,4 +1,4 @@
-const multer = require("multer");
+/**const multer = require("multer");
 const path = require("path")
 
 
@@ -18,5 +18,44 @@ const fileFilter = (req, file, cb) => {
 
 
 const upload = multer({ storage, fileFilter });
+
+module.exports = upload;**/
+
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const uploadDir = path.join(__dirname, "..", "uploads");
+
+// Create uploads folder if it doesn't exist
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "application/pdf") {
+    cb(null, true);
+  } else {
+    cb(new Error("Only PDF allowed"), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+});
 
 module.exports = upload;
