b4289adc65be32d70154ebf2578b4b3f14609246
Nikita Chauhan <nikitachauhan2006@gmail.com>
2026-08-11 21:26:54 +0530

    Fix backend URL

--- MERN_AI/src/utils/axios.js
+++ MERN_AI/src/utils/axios.js
@@ -1,22 +1,5 @@
-/**import axios from'axios';
-
-const instance= axios.create ({
-    baseURL: "https://resume-ai-backend-xn0l.onrender.com",
-    timeout: 5000,
-    headers: {'Content-Type':'application/json'}
-})
-
-export default instance;**/
-
-
-import axios from "axios";
-
 const instance = axios.create({
   baseURL: "https://resume-ai-backend-xn0l.onrender.com",
-  /**timeout: 120000,
-  headers: {
-    "Content-Type": "application/json",
-  },**/
 });
 
 export default instance;
