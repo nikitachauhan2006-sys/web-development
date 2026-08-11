687429ca3579eb6942669da68df298b09acaef0d
Nikita Chauhan <nikitachauhan2006@gmail.com>
2026-08-11 21:45:53 +0530

    Fix CORS for Vercel frontend

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,27 @@
 require("dotenv").config();
 
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
+
 const app = express();
 const PORT = 4000;
 
 const connectDB = require("./conn");
+
 connectDB();
 
 app.use(express.json());
-app.use(cors({
-  credentials: true,
-  origin: "http://localhost:5173"
-}))
+
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      "http://localhost:5173",
+      "https://web-development-yqz2-git-main-nikita-c2ec.vercel.app",
+      "https://web-development-6q3z.vercel.app",
+    ],
+  })
+);
 
 const UserRoutes = require("./Routes/user");
 app.use("/api/user", UserRoutes);
